11cba54e617130eecd39c8e17e757244036519f0
rod007vrr <rod007vrr@gmail.com>
2020-12-20 23:43:02 -0500

    03

--- 03/day3A.py
+++ 03/day3A.py
@@ -3,13 +3,12 @@ with open(r"03\data.txt", 'r') as file:
 
 count = 0
 x = 0
-size = len(data[0])
+size = len(data[0])-1
 for n in data:
     if n[x] == "#":
         count += 1
-    if x  > len(n):
-        x = x+3 - len(n)
-    else:
-        x = x+3
+    x = (x+3) % size
+    
+    
 
 print(count)
